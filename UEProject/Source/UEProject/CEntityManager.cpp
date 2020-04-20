e7e631f48488dfd13a1d66bee203c68033e6e1e0
Philip Lindh <philip.lindh98@hotmail.com>
2020-04-20 12:26:38 +0200

    AI Base Classes

---
+++ UEProject/Source/UEProject/CEntityManager.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CEntityManager.h"
+
+CEntityManager::CEntityManager()
+{
+}
+
+CEntityManager::~CEntityManager()
+{
+}
